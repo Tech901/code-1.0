f5974b02ffe8e56d22f014556f12a72bf2ff3740
Brad Montgomery <brad@bradmontgomery.net>
2024-09-25 18:50:34 -0500

    Update floats.c for 2024

--- examples/intro_to_c/floats.c
+++ examples/intro_to_c/floats.c
@@ -1,19 +1,45 @@
-/**
- * This program illustrates the problem with floating point arithmetic
- * (relevant for the Cash assignment)
- */
+/*
+
+This program illustrates a problem with
+float point arithmetic in C.
+
+*/
 
 #include <stdio.h>
 
-int main(void) {
-    int i = 0;
+int main(void)
+{
+    int counter = 0; // counter variable.
     float value = 10;
-    printf("%d: %f\n", i, value);
 
-    while(value > 0) {
+    printf("%i: %f\n", counter, value);
+
+    while (value > 0)
+    {
         value = value - 0.1;
-        printf("%d: %f\n", i, value);
-        i++;
+        if(value >= 0.1) {
+            printf("%i: %.1f\n", counter, value);
+        } else {
+            printf("%i: %.8f\n", counter, value);
+        }
+
+        counter++;
     }
-    return 0;
+
+    // Thinking in terms of dollars.
+    float money1 = 0.27;
+    float money2 = 12.32;
+    printf("(float) money1 = %f, money2 = %f\n", money1, money2);
+
+    // Think in terms of cents.
+    int money3 = 27;
+    int money4 = 1232;
+
+    // Goal: Show the above as dollars & cents.
+    // type coercion -- (int / float = float)
+    printf("(int) money3 = %.2f, money4 = %.2f\n", money3 / 100.0, money4 / 100.0);
+
+    // type casting -- we explicitly tell the compiler to treat something as a float
+    printf("(int) money3 = %.2f, money4 = %.2f\n",  ((float) money3 / 100), ((float) money4 / 100));
+
 }
