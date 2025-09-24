5d4612e9781c39d33a69adaf9010ff4aaa0e4c05
Brad Montgomery <brad@bradmontgomery.net>
2025-09-24 17:38:30 -0500

    Enhance comments in modulo.c for clarity
    Added comments to explain user input and mod operator behavior.

--- examples/arrays/modulo.c
+++ examples/arrays/modulo.c
@@ -10,9 +10,25 @@
 
 int main(void)
 {
+    // Ask the user for a number...
     int value = get_int("Enter a number: ");
 
+    // What is the behavior of the mod operator?
     for(int i=0; i <= 10; i++) {
         printf("%d mod %d = %d\n", i, value, i % value);
     }
+
+
+    // How does mod impact the *index* when looking up items in an array?
+    /*
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    for(int i=0; i < 26; i++) {
+        if(i % value == 0) {
+            printf("%c, ", letters[i]);
+        }
+        printf("\n");
+    }
+    */
 }
